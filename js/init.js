14a39f5d1aa6d50b94fe913d7d23892c9f2dd5e3
velv <33152370+velv@users.noreply.github.com>
2017-10-28 00:19:44 +0300

    init commit

---
+++ js/init.js
@@ -0,0 +1,9 @@
+$(window).load(function(){
+	var effect = function(){
+	return $('.loader').fadeIn( 350 ).delay( 350 ).fadeOut(600);};
+ $.when( effect() ).done(function() {
+ 	 $('.fade').removeClass('out');
+ 	$('.loader').remove();});});
+$(document).ready(function(){
+	
+});
