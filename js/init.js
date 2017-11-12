7ba2fc5f4860d1158cf68367f9f55aa98b024f35
velv <33152370+velv@users.noreply.github.com>
2017-11-12 18:45:19 +0300

    new design

--- js/init.js
+++
@@ -1,9 +0,0 @@
-$(window).load(function(){
-	var effect = function(){
-	return $('.loader').fadeIn( 350 ).delay( 350 ).fadeOut(600);};
- $.when( effect() ).done(function() {
- 	 $('.fade').removeClass('out');
- 	$('.loader').remove();});});
-$(document).ready(function(){
-	
-});
