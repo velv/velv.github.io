9d2821770f0391bd5404a9174a933dbf37cec775
velv <33152370+velv@users.noreply.github.com>
2017-11-13 14:18:20 +0300

    hot fix

--- js/init.js
+++ js/init.js
@@ -6,23 +6,5 @@ $(window).load(function(){
  	$('.loader').remove();});});
 $(document).ready(function()
 {
-	
-	$(".bg div").click(function () {
-		var sidebar = $(".bg div");
-		
-		if ($(this).hasClass("opened")) {
-			$(".upper__card").animate({
-				left: 0
-			}, 700, function () {
-				sidebar.removeClass("opened");
-			});
-		} else {
-			$(".upper__card").animate({
-				left: "-150%"
-			}, 700, function () {
-				sidebar.addClass("opened");
-			});
-		}
-		
-	});
+
 });
