59a278aba329220a17c6398b4884e61c29f3e202
velv <33152370+velv@users.noreply.github.com>
2017-11-13 03:38:01 +0300

    Revert "new design"
    This reverts commit fa7ee2bc16add81b8ee88987fdbd4389dbf46c9c.

--- js/init.js
+++ js/init.js
@@ -4,25 +4,6 @@ $(window).load(function(){
  $.when( effect() ).done(function() {
  	 $('.fade').removeClass('out');
  	$('.loader').remove();});});
-$(document).ready(function()
-{
+$(document).ready(function(){
 	
-	$(".bg div").click(function () {
-		var sidebar = $(".bg div");
-		
-		if ($(this).hasClass("opened")) {
-			$(".upper__card").animate({
-				left: 0
-			}, 700, function () {
-				sidebar.removeClass("opened");
-			});
-		} else {
-			$(".upper__card").animate({
-				left: "-150%"
-			}, 700, function () {
-				sidebar.addClass("opened");
-			});
-		}
-		
-	});
 });
